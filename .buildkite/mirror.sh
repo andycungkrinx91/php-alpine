4790f6f158a5d83f5690837bb98bc212262d3ce9
andycungkrinx91 <andy.silva270114@gmail.com>
2021-10-19 01:36:21 +0700

    add mirroring

---
+++ .buildkite/mirror.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+ts=$(date +%s)
+githead=$(git rev-parse HEAD)
+branch=$githead-$ts
+echo "Create local branch with tag " $branch
+git clone git@github.com-andycungkrinx91:andycungkrinx91/php-alpine.git $branch
+cd $branch
+git checkout -b $branch
+git remote add upstream git@gitlab.com:andycungkrinx/php-alpine.git
+git pull upstream master --allow-unrelated-histories
+git checkout master
+git merge $branch
+git commit -m "bump repo $branch"
+git push origin master
