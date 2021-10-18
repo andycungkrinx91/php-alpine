ff99f728a0f96fb76cc40e2a12b97df2eda2895e
andycungkrinx91 <andy.silva270114@gmail.com>
2021-10-19 01:44:41 +0700

    update mirroring

--- .buildkite/mirror.sh
+++ .buildkite/mirror.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-git config --global user.name "andycungkrinx91"
 ts=$(date +%s)
 githead=$(git rev-parse HEAD)
 branch=$githead-$ts
 echo "Create local branch with tag " $branch
+git config --global user.name "andycungkrinx"
 git clone git@github.com-andycungkrinx91:andycungkrinx91/php-alpine.git $branch
 cd $branch
 git checkout -b $branch
